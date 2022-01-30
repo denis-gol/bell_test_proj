74b28c0e726906c0bb75cc54f29793cab73ed8b9
denisg <dagol.1980@gmail.com>
2022-01-30 18:37:53 +0300

    add fixtures

--- apps/site.local/src/DataFixtures/AuthorFixtures.php
+++ apps/site.local/src/DataFixtures/AuthorFixtures.php
@@ -2,17 +2,25 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Author;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AuthorFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('ru_RU');
 
+        // f.e. "1 | Лев Толстой"
+        for ($i = 0; $i < 20; $i++) {
+            $author = new Author();
+            $authorName = $faker->firstName(). ' '. $faker->lastName();
 
+            $author->setName($authorName);
+            $manager->persist($author);
+        }
 
         $manager->flush();
     }
