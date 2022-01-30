0160c22f00e52afcab0fc4d04e6c9ce9e17b337a
denisg <dagol.1980@gmail.com>
2022-01-31 00:26:55 +0300

    simple phpunit test (author, add/remove books)

---
+++ apps/site.local/tests/AuthorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use PHPUnit\Framework\TestCase;
+
+class AuthorTest extends TestCase
+{
+    private Author $author;
+
+    public function setUp(): void
+    {
+        $this->author = new Author();
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetEmptyCollectionFromNewAuthor()
+    {
+        $this->assertEmpty($this->author->getBooks());
+    }
+
+    public function testAddBookToAuthor()
+    {
+        $book = (new Book())->setName('Лев Толстой');
+        $this->author->addBook($book);
+
+        $this->assertEquals(
+            $this->author->getBooks()->first()->getName(),
+            'Лев Толстой'
+        );
+    }
+
+    public function testDeleteBookBelongsAuthor()
+    {
+        $book = (new Book())->setName('testname');
+        $this->author->addBook($book);
+
+        $this->author->removeBook($book);
+
+        $this->assertEmpty($this->author->getBooks());
+    }
+
+}
